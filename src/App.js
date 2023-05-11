9d7131b1c354ade943a1596d867f223550b8f834
Arthur Fernandez <arthur@snooget.com>
2023-05-11 09:24:34 -0700

    organized components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   useLocation,
 } from 'react-router-dom';
+import Home from './scenes/home/Home'; // Folder does not exist yet
 
 const scrollToTop = () => {
   const { pathname } = useLocation();
@@ -17,7 +18,14 @@ const scrollToTop = () => {
 }; // This function makes it so that users start at the top of a new page when clicking a link.
 
 function App() {
-  return <div className="app"></div>
+  return <div className="app">
+    <BrowserRouter>
+      <ScrollToTop/> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+  </div>
 }
 
 export default App;
