1ad1c9a119567cfc37981030418ee6d90c050da0
Arthur Fernandez <arthur@snooget.com>
2023-05-11 08:35:20 -0700

    added a scroll to top feature with react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div className="app">
+import { useAffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 
-    </div>
-  );
+const scrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useAffect(()=>{
+    window.scrollTo(0, 0);
+  }, [[pathname]])
+
+  return null;
+}; // This function makes it so that users start at the top of a new page when clicking a link.
+
+function App() {
+  return <div className="app"></div>
 }
 
 export default App;
