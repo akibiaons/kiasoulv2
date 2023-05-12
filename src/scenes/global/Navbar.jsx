a7140b054ffd8c75ead11876d7d64acfe096b5c2
Arthur Fernandez <arthur@snooget.com>
2023-05-11 18:55:43 -0700

    adding cart menu

--- src/scenes/global/Navbar.jsx
+++ src/scenes/global/Navbar.jsx
@@ -5,15 +5,16 @@ import {
     ShoppingBagOutlined,
     MenuOutlined,
     SearchOutlined,
-    PersonOutlined
 } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { shades } from '../../theme';
+import { setIsCartOpen } from "../../state";
 
 
 const Navbar = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const cart = useSelector((state.cart.cart)) // duplicate to grab the index.js/state slice and state above it..
 
     // material ui lets you pass in CSS properties directly into the <Box> component specifically.
     return (
@@ -55,9 +56,27 @@ const Navbar = () => {
                     <IconButton sx={{ color: "black" }}>
                         <PersonOutline />
                     </IconButton>
-                    <IconButton sx={{ color: "black" }}>
-                        <ShoppingBagOutlined />
-                    </IconButton>
+
+                    <Badge
+                        badgeContent={cart.length}
+                        color="secondary"
+                        indvisible={cart.length === 0}
+                        sx={{
+                            "& .MuiBadge-badge": {
+                                right: 5,
+                                top: 5,
+                                padding: "0 4px",
+                                height: "14px",
+                                minWidth: "13px",
+                            }
+                        }}
+                    >
+                        <IconButton
+                            onClick={() => dispatch(setIsCartOpen({}))}
+                            sx={{ color: "black" }}>
+                            <ShoppingBagOutlined />
+                        </IconButton>
+                    </Badge>
                     <IconButton sx={{ color: "black" }}>
                         <MenuOutlined />
                     </IconButton>
