45545fe84d637539ae2aea4967ca6a0f054d9ab7
Arthur Fernandez <arthur@snooget.com>
2023-05-11 18:36:12 -0700

    added all the redux state

--- src/state/index.js
+++ src/state/index.js
@@ -6,3 +6,45 @@ const initialState = {
     cart: [],
     items: [],
 };
+// redix toolkit allows you to mutate state, even though it is not a good practice.
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        setItems: (state, action) => {
+            state.items = action.payload;
+        },
+        // Function below changed what is in the "cart"
+        addToCart: (state, action) => {
+            state.cart = [...state.cart, action.payload.item]; // What ever item is passed into the action will update the cart.
+        },
+
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter((item) => item.id !== action.payload.id); // This lets you remove items from the cart by removing items without the id's we are passing in.
+        },
+
+        increaseCount: (state, action) => {
+            state.cart = state.cart.map((item) => {
+                if (item.id === action.payload.id && item.count > 1) {
+                    item.count--;
+                }
+                return item;
+            });
+        },
+
+        setIsCartOpen: (state) => {
+            state.isCartOpen = !state.isCartOpen;
+        }
+    }
+});
+
+export const {
+    setItems,
+    addToCart,
+    removeFromCart,
+    increaseCount,
+    decreaseCount,
+    setIsCartOpen,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
