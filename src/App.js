32c0c1a3b532ab841446f532c276b35833ef7cee
Arthur Fernandez <arthur@snooget.com>
2023-05-11 17:36:56 -0700

    added confirmation component & routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   useLocation,
 } from 'react-router-dom';
 import Home from './scenes/home/Home'; // Folder does not exist yet
+import ItemDetails from './scenes/itemDetails/ItemDetails';
 
 const scrollToTop = () => {
   const { pathname } = useLocation();
@@ -18,14 +19,18 @@ const scrollToTop = () => {
 }; // This function makes it so that users start at the top of a new page when clicking a link.
 
 function App() {
-  return <div className="app">
-    <BrowserRouter>
-      <ScrollToTop/> 
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-    </BrowserRouter>
-  </div>
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop /> 
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="item/:itemId" element={<ItemDetails />} />
+            <Route path="checkout" element={<Checkout />} />        
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
