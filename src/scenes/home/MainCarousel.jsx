396b6e4342cfc7f6a73a72090f8050d26dca82bd
Arthur Fernandez <arthur@snooget.com>
2023-05-12 19:22:11 -0700

    added maincarouselx

---
+++ src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,23 @@
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-repsonsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { shades } from "../../theme";
+
+//const variable below imports all images from assets folder.
+const importAll = (r) => 
+    r.keys.reduce((acc, item) => {
+        acc[item.replace("./", "")] = r(item);
+        return acc;
+    }, []); // He said arr but {} is object brackets, may ned fixing later.
+
+export const heroTextureImports = importAll(
+    require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
+)
+
+const MainCarousel = () => {
+    const isNonMobile = useMediaQuery ("(min-width: 600px)") // Might be missing a paretnthesis.
+};
+
+export default MainCarousel;
