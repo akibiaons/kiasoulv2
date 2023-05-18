3f61f157077a173f201ce4c02ec5599b0de5589c
Arthur Fernandez <arthur@snooget.com>
2023-05-18 07:45:27 -0700

    added root to index

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,14 +1,14 @@
-import MainCarousel from "./MainCarousel"
+import MainCarousel from "./MainCarousel";
 import ShoppingList from "./ShoppingList";
 import subscribe from "./Subscribe";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <MainCarousel />
-            <ShoppingList />
-        </div>
-    );
+  return (
+    <div className="home">
+      <MainCarousel />
+      <ShoppingList />
+    </div>
+  );
 };
 
 export default Home;
