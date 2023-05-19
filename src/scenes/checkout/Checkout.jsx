e316034ae738ee32cf228cdf07dd754823959a98
Arthur Fernandez <arthur@snooget.com>
2023-05-19 07:29:08 -0700

    added stripe function and function call to redire

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -124,6 +124,16 @@ const Checkout = () => {
         count,
       })),
     };
+
+    const response = await fetch("http://localhost:1337/api/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
+    });
+    const session = await response.json();
+    await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
   }
 
   return (
