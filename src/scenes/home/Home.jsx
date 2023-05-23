0ea2004464df2e1f1808edc6e441c810a925eedf
Arthur Fernandez <arthur@snooget.com>
2023-05-23 08:24:01 -0700

    edited home component

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,12 +1,13 @@
 import MainCarousel from "./MainCarousel";
 import ShoppingList from "./ShoppingList";
-import subscribe from "./Subscribe";
+import Subscribe from "./Subscribe";
 
 const Home = () => {
   return (
     <div className="home">
       <MainCarousel />
       <ShoppingList />
+      <Subscribe />
     </div>
   );
 };
